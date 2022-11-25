bf6fac025e37fd2b60b0b6c7d78cd3092b72585d
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-25 19:14:05 +0100

    remove cachable

--- src/main/java/pl/lottery/lotterygenerator/AppRunner.java
+++ src/main/java/pl/lottery/lotterygenerator/AppRunner.java
@@ -2,10 +2,8 @@ package pl.lottery.lotterygenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class AppRunner {
 
 	public static void main(String[] args) {
